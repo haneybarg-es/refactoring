90bc13690ffd3ede01f144bbc902d2254e6d3638
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-05-26 22:22:33 -0300

    COMMIT & PUSH 9.

--- Movie.java
+++ Movie.java
@@ -5,22 +5,42 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+  public Movie(String name, int priceCode) {
+    _title = name;
+      setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
-  }
+    return _price.getPriceCode();
+   }
 
   public void setPriceCode(int arg) {
-     _priceCode = arg;
+    switch (arg) {
+    case REGULAR:
+      _price = new RegularPrice();
+      break;
+    case CHILDRENS:
+      _price = new ChildrensPrice();
+      break;
+    case NEW_RELEASE:
+      _price = new NewReleasePrice();
+      break;
+    default:
+      throw new IllegalArgumentException("Incorrect Price Code");
+    }
+  }
+
+  double getCharge(int daysRented) {
+    return _price.getCharge(daysRented);
+   }
+
+  int getFrequentRenterPoints(int daysRented) {
+    return _price.getFrequentRenterPoints(daysRented);
   }
 
   public String getTitle (){
-      return _title;
+    return _title;
   };
 }
