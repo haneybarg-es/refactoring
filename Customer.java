d62409baea76302b30ac512deb1536ae91f75ee7
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-05-26 22:08:42 -0300

    COMMIT & PUSH 5.

--- Customer.java
+++ Customer.java
@@ -23,12 +23,7 @@ class Customer {
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) {
-      double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
-
-      //determine amounts for each line
-
-      thisAmount = each.getCharge();
 
       // add frequent renter points
       frequentRenterPoints ++;
@@ -36,16 +31,17 @@ class Customer {
       if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
           each.getDaysRented() > 1) frequentRenterPoints ++;
 
-      //show figures for this rental
-      result += "\t" + each.getMovie().getTitle()+ "\t" +
-        String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+      // show figures for this rental
+      result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
+        (each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
 
     }
-    //add footer lines
+
+    // add footer lines
     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) +
-      " frequent renter points";
+    result += "You earned " + String.valueOf(frequentRenterPoints)
+      + " frequent renter points";
     return result;
   }
 }
